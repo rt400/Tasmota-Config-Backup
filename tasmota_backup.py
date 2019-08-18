5959b7d676dfe074f6c195fbf85fc82240096140
Yuval <38878996+rt400@users.noreply.github.com>
2019-08-19 02:07:24 +0300

    fix backup files

--- tasmota_backup.py
+++ tasmota_backup.py
@@ -1,12 +1,7 @@
-import json
 import socket
-import urllib
-
 import requests
-import wget
 import ipaddress
 import sys
-
 
 if len(sys.argv) == 1:
     print("No IP address entered !")
@@ -15,6 +10,9 @@ elif len(sys.argv) == 3:
     try:
         start_ip = ipaddress.ip_address(str(sys.argv[1]))
         end_ip = ipaddress.ip_address(str(sys.argv[2]))
+        if end_ip < start_ip:
+            print("Wrong IP Range !")
+            exit()
     except:
         print("Wrong IP address !")
         exit()
@@ -30,18 +28,18 @@ for ip_int in range(int(start_ip), int(end_ip + 1)):
     try:
         ip_str = ipaddress.IPv4Address(ip_int).__str__()
         url = 'http://' + ip_str + '/dl?'
-        url_ver = 'http://' + ip_str + '/cm?cmnd=status%202'
+        url_ver = 'http://' + ip_str + '/cm?cmnd=status%200'
         try:
-            #socket.settimeout(2)
-            ip_str = socket.gethostbyaddr(ip_str).__str__()
+            backup_file = requests.get(url, allow_redirects=True, timeout=5)
+            backup_ver = requests.get(url_ver, allow_redirects=True, timeout=5).json()
+            print("Found Tasmota Firmware in " + ip_str)
+            with open(backup_ver['StatusNET']['Hostname'] + '_' + ip_str + '_Ver_' +
+                      backup_ver['StatusFWR']['Version'] + '.dmp', 'wb') as filetowrite:
+                filetowrite.write(backup_file.content)
+                filetowrite.close()
         except:
-            print("No Hostname for " + ip_str + ", using IP")
-        backup_file = requests.get(url, allow_redirects=True, timeout=5)
-        backup_ver = requests.get(url_ver, allow_redirects=True, timeout=5).json()
-        print("Found Tasmota Firmware in " + ip_str)
-        with open(ip_str + '_' + backup_ver['StatusFWR']['Version'] +'.dmp', 'wb') as filetowrite:
-            filetowrite.write(backup_file.content)
-            filetowrite.close()
+            print("Not found TASMOTA firmware in " + ip_str)
+            #print(sys.exc_info())
     except:
-        #print(sys.exc_info())
-        print("Not found TASMOTA firmware in "+ip_str)
+        print(sys.exc_info())
+        print("Not found TASMOTA firmware in " + ip_str)
